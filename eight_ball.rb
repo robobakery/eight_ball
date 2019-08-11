20c9e10b9cdd60c988454495a40f1fed064ed4d0
Jey Choi <j@robobakery.io>
2019-08-11 14:02:21 +0900

    add formula

---
+++ eight_ball.rb
@@ -0,0 +1,17 @@
+class EightBall < Formula
+    desc "EightBall CLI example"
+    homepage "https://github.com/robobakery/eight_ball"
+    url "https://github.com/robobakery/eight_ball/raw/master/releases/eight_ball-0.0.1.tar.gz"
+    sha256 "4bedeb1fe98cc94849e5569a01d71727add51713fe7f4f3e09437f1280e198df"
+    version "0.0.1"
+
+    def install
+        # install eight_ball script, create symlink to script in /usr/local/bin
+        bin.install "eight_ball"
+    end
+
+    test do
+        # test script output
+        assert_equal %x('#{bin}/eight_ball'), "EightBall\n"
+    end
+end
