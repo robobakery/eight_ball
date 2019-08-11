eb61d083ca8ae9b2aa8335baf3660cc1d36b2332
Jey Choi <j@robobakery.io>
2019-08-11 16:16:51 +0900

    one more time

--- eight_ball.rb
+++ eight_ball.rb
@@ -2,7 +2,7 @@ class EightBall < Formula
     desc "EightBall CLI example"
     homepage "https://github.com/robobakery/homebrew-eight_ball"
     url "https://github.com/robobakery/homebrew-eight_ball/raw/master/releases/eight_ball-0.0.2.tar.gz"
-    sha256 "acf1150336994155a737872cbfebc713e5a3f244ee4480fb155385eb76c8f787"
+    sha256 "24438a3f9699c0826c701f95e96c532da9042c785e3394f76688d4067e7cadce"
     version "0.0.2"
 
     def install
