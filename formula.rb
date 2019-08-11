89e843821bd67528801e2b968eb7bbcd19b37cf3
Jey Choi <j@robobakery.io>
2019-08-11 14:10:28 +0900

    update

--- formula.rb
+++ formula.rb
@@ -1,7 +1,7 @@
 class EightBall < Formula
     desc "EightBall CLI example"
     homepage "https://github.com/robobakery/eight_ball"
-    url "https://github.com/robobakery/eight_ball/raw/master/releases/eight_ball-0.0.1.tar.gz"
+    url "https://github.com/robobakery/homebrew-eight_ball/raw/master/releases/eight_ball-0.0.1.tar.gz"
     sha256 "4bedeb1fe98cc94849e5569a01d71727add51713fe7f4f3e09437f1280e198df"
     version "0.0.1"
 
